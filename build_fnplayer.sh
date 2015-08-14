33bf9a10fde3e0f9c8f349058bd368e80bf3a18a
Grant Chen <grantchen@fusionnext.local>
2015-08-14 10:14:53 +0800

    added lib

--- build_fnplayer.sh
+++ build_fnplayer.sh
@@ -1,3 +1,4 @@
 RUN_CLANG_STATIC_ANALYZER=NO xcodebuild -configuration Release -target kxmovie -sdk iphoneos -localizationPath clean build
-RUN_CLANG_STATIC_ANALYZER=NO xcodebuild -configuration Release -target kxmovie -sdk iphonesimulator -localizationPath clean build  
-lipo -create -output libFNPlayer.a build/Release-iphoneos/libkxmovie.a build/Release-iphonesimulator/libkxmovie.a
+RUN_CLANG_STATIC_ANALYZER=NO xcodebuild -configuration Release -target kxmovie -sdk iphonesimulator -localizationPath clean build
+mkdir -p lib
+lipo -create -output lib/libFNPlayer.a build/Release-iphoneos/libkxmovie.a build/Release-iphonesimulator/libkxmovie.a
